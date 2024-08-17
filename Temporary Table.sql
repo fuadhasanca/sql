ff6c38e2cc40b7069597f93f31219e2bc64e8bfb
hasanfuad <hasanmdfuad@hotmail.com>
2024-08-17 01:04:46 -0400

    commit temp and mysql updated syntax

--- Temporary Table.sql
+++ Temporary Table.sql
@@ -1,2 +1,23 @@
---Temporary Table--
-CREATE TEMPORARY TABLE ()
+USE parks_and_recreation;
+
+CREATE TEMPORARY Table temp_table
+(
+first_name varchar(50),
+last_name varchar(50),
+fav_movie varchar(100)
+);
+
+INSERT INTO temp_table VALUES(
+'Fuad', 'Hasan', '3 Idiots'
+);
+
+SELECT *
+FROM temp_table;
+
+CREATE TEMPORARY TABLE SalaryEmployee50k
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+SELECT *
+FROM SalaryEmployee50k;
