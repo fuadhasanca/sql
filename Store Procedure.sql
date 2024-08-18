005492b9719343eb9850026659f2ba3fb5188040
hasanfuad <hasanmdfuad@hotmail.com>
2024-08-17 22:20:15 -0400

    commit procedure with parameter

--- Store Procedure.sql
+++ Store Procedure.sql
@@ -1,19 +1,41 @@
-CREATE PROCEDURE large_salary()
+CREATE PROCEDURE large_salary1()
 SELECT *
 FROM employee_salary
 WHERE salary >= 50000;
 
-CALL large_salary();
+CALL large_salary1()
 
 DELIMITER $$
-CREATE PROCEDURE largest_salaries()
+CREATE PROCEDURE largest_salary()
 BEGIN
-	SELECT *
-	FROM employee_salary
-	WHERE salary >= 50000;
-	SELECT *
-	FROM employee_salary
-	WHERE salary >= 10000;
-END $$
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+SELECT *
+FROM employee_salary
+WHERE salary >= 10000; 
+END
+$$
 
-CALL largest_salaries()
+CALL largest_salary();
+
+
+CREATE PROCEDURE single_person_salary3(param_employee_id INT)
+SELECT *
+FROM employee_salary
+WHERE employee_id = param_employee_id;
+
+CALL single_person_salary3(4);
+
+
+DELIMITER $$
+CREATE PROCEDURE person_salary_by_name(person_name varchar(50))
+BEGIN
+SELECT *
+FROM employee_salary
+WHERE first_name = person_name;
+END
+$$
+
+CALL person_salary_by_name('Tom')
+
